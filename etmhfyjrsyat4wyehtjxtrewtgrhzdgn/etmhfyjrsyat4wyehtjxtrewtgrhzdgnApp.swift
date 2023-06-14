74aa25d65bebe935600d439dd8779019f0a747de
Stellafishe <stellafishes@gmail.com>
2023-06-14 09:29:18 -0500

    Initial Commit

---
+++ etmhfyjrsyat4wyehtjxtrewtgrhzdgn/etmhfyjrsyat4wyehtjxtrewtgrhzdgnApp.swift
@@ -0,0 +1,17 @@
+//
+//  etmhfyjrsyat4wyehtjxtrewtgrhzdgnApp.swift
+//  etmhfyjrsyat4wyehtjxtrewtgrhzdgn
+//
+//  Created by Scholar on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct etmhfyjrsyat4wyehtjxtrewtgrhzdgnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
